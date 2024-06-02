7f6cfc1660472d98c7ab6f0ed31ca56a084ef637
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2024-06-02 18:50:57 +0300

    - Display trending Podcasts - Create PodcastCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import PodcastCard from "@/components/PodcastCard";
 import { Button } from "@/components/ui/button";
+import { podcastData } from "@/constants";
 import React from "react";
 
 const Home = () => {
@@ -6,7 +8,19 @@ const Home = () => {
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        <Button className="text-white-1 bg-orange-1">View All</Button>
+        <div className="podcast_grid">
+          {podcastData.map((podcast) => {
+            return (
+              <PodcastCard
+                key={podcast.id}
+                imageUrl={podcast.imgURL}
+                title={podcast.title}
+                description={podcast.description}
+                id={podcast.id}
+              ></PodcastCard>
+            );
+          })}
+        </div>
       </section>
     </div>
   );
